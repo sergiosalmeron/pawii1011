60fa53966ea3d10152be488f3e2870e6d0e8ee6a
aleixgarrido <aleixgarrido@bab4e3fb-5ab7-ad66-41a8-dd50f87c43ef>
2011-01-27 17:10:36 +0000

    Conexión base de datos y creación de Mensaje y Usuario.

---
+++ trunk/Tabique2/Tabique2-ejb/src/java/entidades/MensajeFacade.java
@@ -0,0 +1,56 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package entidades;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.Root;
+
+/**
+ *
+ * @author usuario_local
+ */
+@Stateless
+public class MensajeFacade extends AbstractFacade<Mensaje> implements MensajeFacadeLocal, MensajeFacadeRemote {
+    @PersistenceContext(unitName = "Tabique2-ejbPU")
+    private EntityManager em;
+
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public MensajeFacade() {
+        super(Mensaje.class);
+    }
+
+      public List<Mensaje> findLOQUESEA() {
+     /*
+      CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+     CriteriaQuery<Employee> criteriaQuery = criteriaBuilder.createQuery(Employee.class);
+     Root<Employee> employee = criteriaQuery.from(Employee.class);
+     Predicate condition = criteriaBuilder.gt(employee.get(Employee_.age), 24);
+     criteriaQuery.where(condition);
+     TypedQuery<Employee> typedQuery = em.createQuery(criteriaQuery);
+     List<Employee> result = typedQuery.getResultList();
+     */
+     //Corresponding SQL: SELECT * FROM employee WHERE age > 24
+
+        javax.persistence.criteria.CriteriaQuery cq =
+                getEntityManager().getCriteriaBuilder().createQuery(Mensaje.class);
+        Root<Mensaje> messageinstances = cq.from(Mensaje.class);
+        javax.persistence.criteria.Predicate condition = getEntityManager().getCriteriaBuilder().equal(
+                messageinstances.get( "mensaje"),"uola2");
+        cq.where(condition);
+        /* EJQL
+        String query="SELECT * from WHERE ";
+        return getEntityManager().createQuery(query).getResultList();
+*/
+        return getEntityManager().createQuery(cq).getResultList();
+    }
+
+}
