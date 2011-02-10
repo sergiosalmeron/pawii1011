3f6029ad08b5df62048f84d4ea8ed786b916978f
netsergiosm <netsergiosm@bab4e3fb-5ab7-ad66-41a8-dd50f87c43ef>
2011-02-10 02:51:37 +0000

    drag and drop implementado

--- trunk/Tabique2/Tabique2-war/web/JS/funciones.js
+++ trunk/Tabique2/Tabique2-war/web/JS/funciones.js
@@ -1,36 +1,53 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
-<script language="JavaScript1.2">
-    <!--
     var dragapproved=false
-    var z,x,y
-    function move(){
-        if (event.button==1&&dragapproved){
-            z.style.pixelLeft=temp1+event.clientX-x
-            z.style.pixelTop=temp2+event.clientY-y
-            return false
+    var z
+
+    function drags(e){
+
+        var targ
+        if (!e) var e = window.event
+        if (e.target) targ = e.target
+        else if (e.srcElement) targ = e.srcElement
+        if (targ.nodeType == 3) // defeat Safari bug
+        targ = targ.parentNode
+
+
+
+        if (targ.className=="userPapelera"){
+            dragapproved=true
+            z=targ
+           // x=event.clientX
+            //y=event.clientY
+           // document.onmousemove=move
         }
     }
 
-    function drags(){
-        if (!document.all)
-            return
-        if (event.srcElement.className=="drag"){
-            dragapproved=true
-            z=event.srcElement
-            temp1=z.style.pixelLeft
-            temp2=z.style.pixelTop
-            x=event.clientX
-            y=event.clientY
-            document.onmousemove=move
+    function drops(e){
+        var targ
+        if (!e) var e = window.event
+        if (e.target) targ = e.target
+        else if (e.srcElement) targ = e.srcElement
+        if (targ.nodeType == 3) // defeat Safari bug
+        targ = targ.parentNode
+
+        if (dragapproved){
+            if (targ.className=="papelera"){
+                formu=document.forms['formPapelera']
+                atri=formu.elements["viejoUsuario"]
+                atri.value = z.id
+                formu.submit();
+               //document.formPapelera.viejoUsuario = z.id
+               //document.formPapelera.submit()
+
+
+            }
+            z=null
+            dragapproved=false
         }
+    }
 
 
     document.onmousedown=drags
-    document.onmouseup=new Function("dragapproved=false")
-    //-->
-</script>
+    document.onmouseup=drops
+
 
